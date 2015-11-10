fda20fb27c7df820a4d515da9d84f71d3de0bed6
suresh <suresh@workspaceafrica.com>
2015-11-10 16:54:24 +0200

    Fix make

---
+++ sp/com.workspaceafrica.bsol.client/src/main/c/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 10 Nov 2015
+ *      Author: suresh
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
+
